2e312407b6da9eff2e66101d5def0725351bc005
Nara <narawit.tub@outlook.co.th>
2022-02-04 22:55:54 +0700

    Feat: AppointmentsDayView for a list of appointments

--- appointments/src/Appointment.js
+++ appointments/src/Appointment.js
@@ -1,5 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+
+const appointmentTimeOfDay = (startAt) => {
+  const [h, m] = new Date(startAt).toTimeString().split(":");
+  return `${h}:${m}`;
+};
 
 export const Appointment = ({ customer: { firstName } }) => (
   <div>{firstName}</div>
 );
+
+export const AppointmentsDayView = ({ appointments }) => {
+  const [selectedAppointment, setSelectedAppointment] = useState(0);
+  return (
+    <div id="appointments-DayView">
+      <ol>
+        {appointments.map((appointment, index) => (
+          <li key={appointment.startAt}>
+            <button type="button" onClick={() => setSelectedAppointment(index)}>
+              {appointmentTimeOfDay(appointment.startAt)}
+            </button>
+          </li>
+        ))}
+      </ol>
+      {appointments.length === 0 ? (
+        <p>There are no appointments scheduled for today.</p>
+      ) : (
+        <Appointment {...appointments[selectedAppointment]} />
+      )}
+    </div>
+  );
+};
--- appointments/test/Appointment.test.js
+++ appointments/test/Appointment.test.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Appointment } from "../src/Appointment";
+import { Appointment, AppointmentsDayView } from "../src/Appointment";
+import ReactTestUtils from "react-dom/test-utils";
+
 describe("Appointment", () => {
   let customer;
   let container;
@@ -22,6 +24,68 @@ describe("Appointment", () => {
     customer = { firstName: "Sarisa" };
 
     render(<Appointment customer={customer} />);
+
+    expect(container.textContent).toMatch("Sarisa");
+  });
+});
+
+describe("AppointmentsDayView", () => {
+  let container;
+  let appointments;
+
+  beforeEach(() => {
+    container = document.createElement("div");
+    const today = new Date();
+    appointments = [
+      { startAt: today.setHours(12, 0), customer: { firstName: "Nara" } },
+      { startAt: today.setHours(13, 0), customer: { firstName: "Sarisa" } },
+    ];
+  });
+
+  const render = (component) => ReactDOM.render(component, container);
+
+  it("initially shows a message saying there are no appointments today", () => {
+    render(<AppointmentsDayView appointments={[]} />);
+    expect(container.textContent).toMatch(
+      "There are no appointments scheduled for today."
+    );
+  });
+
+  it("selects the first appointments by default", () => {
+    render(<AppointmentsDayView appointments={appointments} />);
+    expect(container.textContent).toMatch("Nara");
+  });
+
+  it("renders a div with the right id", () => {
+    render(<AppointmentsDayView appointments={[]} />);
+    expect(container.querySelector("div#appointments-DayView")).not.toBeNull();
+  });
+
+  it("renders multiple appointments in an ol element", () => {
+    render(<AppointmentsDayView appointments={appointments} />);
+
+    expect(container.querySelector("ol")).not.toBeNull();
+    expect(container.querySelector("ol").children).toHaveLength(2);
+  });
+
+  it("renders each appointment in an li", () => {
+    render(<AppointmentsDayView appointments={appointments} />);
+
+    expect(container.querySelectorAll("li")).toHaveLength(2);
+    expect(container.querySelectorAll("li")[0].textContent).toEqual("12:00");
+    expect(container.querySelectorAll("li")[1].textContent).toEqual("13:00");
+  });
+  it("has a button element in each li", () => {
+    render(<AppointmentsDayView appointments={appointments} />);
+
+    expect(container.querySelectorAll("li > button")).toHaveLength(2);
+    expect(container.querySelectorAll("li > button")[0].type).toEqual("button");
+  });
+  it("renders another appointment when selected", () => {
+    render(<AppointmentsDayView appointments={appointments} />);
+
+    const button = container.querySelectorAll("button")[1];
+    ReactTestUtils.Simulate.click(button);
 
     expect(container.textContent).toMatch("Sarisa");
   });
