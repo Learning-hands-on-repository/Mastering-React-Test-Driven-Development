c34f7b14305d78c5b42091eb408030f2b0f5778a
Nara <narawit.tub@outlook.co.th>
2022-02-10 22:49:36 +0700

    <CustomerForm />

--- appointments/src/CustomerForm.js
+++ appointments/src/CustomerForm.js
@@ -1,3 +1,14 @@
 import React from "react";
 
-export const CustomerForm = () => <form id="customer" />;
+export const CustomerForm = ({ firstName }) => (
+  <form id="customer">
+    <label htmlFor="firstName">First Name</label>
+    <input
+      type="text"
+      name="firstName"
+      value={firstName}
+      readOnly
+      id="firstName"
+    />
+  </form>
+);
--- appointments/test/CustomerForm.test.js
+++ appointments/test/CustomerForm.test.js
@@ -4,14 +4,44 @@ import { CustomerForm } from "../src/CustomerForm";
 
 describe("CustomerForm", () => {
   let render, container;
+  const form = (id) => container.querySelector(`form[id="${id}"]`);
+
+  const firstNameField = () => form("customer").elements.firstName;
+
+  const expectToBeInputFieldOfTypeText = (formElement) => {
+    expect(formElement).not.toBeNull();
+    expect(formElement.tagName).toEqual("INPUT");
+    expect(formElement.type).toEqual("text");
+  };
+
+  const labelFor = (formElement) => {
+    return container.querySelector(`label[for="${formElement}"]`);
+  };
 
   beforeEach(() => {
     ({ render, container } = createContainer());
   });
-  const form = (id) => container.querySelector(`form[id="${id}"]`);
 
-  it("renders a form", () => {
+  it("renders the first name field as a text box", () => {
     render(<CustomerForm />);
-    expect(form("customer")).not.toBeNull();
+
+    expectToBeInputFieldOfTypeText(firstNameField());
+  });
+
+  it("includes the existing value for the first name", () => {
+    render(<CustomerForm firstName="Nara" />);
+
+    expect(firstNameField().value).toEqual("Nara");
+  });
+
+  it("renders a label for the first name field", () => {
+    render(<CustomerForm />);
+    expect(labelFor("firstName")).not.toBeNull();
+    expect(labelFor("firstName").textContent).toEqual("First Name");
+  });
+
+  it("assigns an id that matches the label id to the first name", () => {
+    render(<CustomerForm />);
+    expect(firstNameField().id).toEqual("firstName");
   });
 });
