5de953fa5a8c51d59acc13d343b342fe890d55a5
Nara <narawit.tub@outlook.co.th>
2022-02-17 22:51:22 +0700

    add timesSlotTable component

--- appointments/src/AppointmentForm.js
+++ appointments/src/AppointmentForm.js
@@ -1,6 +1,62 @@
 import React, { useState } from "react";
 
-export const AppointmentForm = ({ selectableServices, service, onSubmit }) => {
+const dailyTimeSlots = (salonOpensAt, salonClosesAt) => {
+  const totalSlots = (salonClosesAt - salonOpensAt) * 2;
+  const startTime = new Date().setHours(salonOpensAt, 0, 0, 0);
+  const increment = 30 * 60 * 1000;
+  return Array(totalSlots)
+    .fill([startTime])
+    .reduce((acc, _, i) => acc.concat([startTime + i * increment]));
+};
+
+const toTimeValue = (timestamp) =>
+  new Date(timestamp).toTimeString().substring(0, 5);
+
+const weeklyDateValues = (startDate) => {
+  const midnight = new Date(startDate).setHours(0, 0, 0, 0);
+  const increment = 24 * 60 * 60 * 1000;
+  return Array(7)
+    .fill([midnight])
+    .reduce((acc, _, i) => acc.concat([midnight + i * increment]));
+};
+
+const toShortDate = (timestamp) => {
+  const [day, , dayOfMonth] = new Date(timestamp).toDateString().split(" ");
+  return `${day} ${dayOfMonth}`;
+};
+
+const TimeSlotTable = ({ salonOpensAt, salonClosesAt, today }) => {
+  const timeSlots = dailyTimeSlots(salonOpensAt, salonClosesAt);
+  const dates = weeklyDateValues(today);
+  return (
+    <table id="time-slots">
+      <thead>
+        <tr>
+          <th />
+          {dates.map((d) => (
+            <th key={d}>{toShortDate(d)}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {timeSlots.map((timeSlot) => (
+          <tr key={timeSlot}>
+            <th>{toTimeValue(timeSlot)}</th>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export const AppointmentForm = ({
+  selectableServices,
+  service,
+  onSubmit,
+  salonOpensAt,
+  salonClosesAt,
+  today,
+}) => {
   const [appointment, setAppointment] = useState({ service });
 
   const handleChange = ({ target }) => {
@@ -25,6 +81,11 @@ export const AppointmentForm = ({ selectableServices, service, onSubmit }) => {
           <option key={s}>{s}</option>
         ))}
       </select>
+      <TimeSlotTable
+        salonOpensAt={salonOpensAt}
+        salonClosesAt={salonClosesAt}
+        today={today}
+      />
     </form>
   );
 };
@@ -38,4 +99,7 @@ AppointmentForm.defaultProps = {
     "Cut & bread trim",
     "Extensions",
   ],
+  salonOpensAt: 9,
+  salonClosesAt: 19,
+  today: new Date(),
 };
