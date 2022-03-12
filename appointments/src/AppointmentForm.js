abfc2331e0da824035ef46d188d30e2730755dce
Nara <narawit.tub@outlook.co.th>
2022-03-12 21:19:54 +0700

    I dont know what change I've made

--- appointments/src/AppointmentForm.js
+++ appointments/src/AppointmentForm.js
@@ -1,31 +1,58 @@
 import React, { useState } from "react";
 
+const timeIncrements = (numTimes, startTime, increment) =>
+  Array(numTimes)
+    .fill([startTime])
+    .reduce((acc, _, i) => acc.concat([startTime + i * increment]));
+
 const dailyTimeSlots = (salonOpensAt, salonClosesAt) => {
   const totalSlots = (salonClosesAt - salonOpensAt) * 2;
   const startTime = new Date().setHours(salonOpensAt, 0, 0, 0);
   const increment = 30 * 60 * 1000;
-  return Array(totalSlots)
-    .fill([startTime])
-    .reduce((acc, _, i) => acc.concat([startTime + i * increment]));
+  return timeIncrements(totalSlots, startTime, increment);
 };
-
-const toTimeValue = (timestamp) =>
-  new Date(timestamp).toTimeString().substring(0, 5);
 
 const weeklyDateValues = (startDate) => {
   const midnight = new Date(startDate).setHours(0, 0, 0, 0);
   const increment = 24 * 60 * 60 * 1000;
-  return Array(7)
-    .fill([midnight])
-    .reduce((acc, _, i) => acc.concat([midnight + i * increment]));
+  return timeIncrements(7, midnight, increment);
 };
+
+const toTimeValue = (timestamp) =>
+  new Date(timestamp).toTimeString().substring(0, 5);
 
 const toShortDate = (timestamp) => {
   const [day, , dayOfMonth] = new Date(timestamp).toDateString().split(" ");
   return `${day} ${dayOfMonth}`;
 };
 
-const TimeSlotTable = ({ salonOpensAt, salonClosesAt, today }) => {
+const mergeDateAndTime = (date, timeSlot) => {
+  const time = new Date(timeSlot);
+  return new Date(date).setHours(
+    time.getHours(),
+    time.getMinutes(),
+    time.getSeconds(),
+    time.getMilliseconds()
+  );
+};
+const RadioButtonIfAvailable = ({ availableTimeSlots, date, timeSlot }) => {
+  const startsAt = mergeDateAndTime(date, timeSlot);
+  if (
+    availableTimeSlots.some(
+      (availableTimeSlot) => availableTimeSlot.startsAt === startsAt
+    )
+  ) {
+    return <input name="startsAt" type="radio" value={startsAt} />;
+  }
+  return null;
+};
+
+const TimeSlotTable = ({
+  salonOpensAt,
+  salonClosesAt,
+  today,
+  availableTimeSlots,
+}) => {
   const timeSlots = dailyTimeSlots(salonOpensAt, salonClosesAt);
   const dates = weeklyDateValues(today);
   return (
@@ -42,6 +69,15 @@ const TimeSlotTable = ({ salonOpensAt, salonClosesAt, today }) => {
         {timeSlots.map((timeSlot) => (
           <tr key={timeSlot}>
             <th>{toTimeValue(timeSlot)}</th>
+            {dates.map((date) => (
+              <td key={date}>
+                <RadioButtonIfAvailable
+                  availableTimeSlots={availableTimeSlots}
+                  date={date}
+                  timeSlot={timeSlot}
+                />
+              </td>
+            ))}
           </tr>
         ))}
       </tbody>
@@ -56,6 +92,7 @@ export const AppointmentForm = ({
   salonOpensAt,
   salonClosesAt,
   today,
+  availableTimeSlots,
 }) => {
   const [appointment, setAppointment] = useState({ service });
 
@@ -85,6 +122,7 @@ export const AppointmentForm = ({
         salonOpensAt={salonOpensAt}
         salonClosesAt={salonClosesAt}
         today={today}
+        availableTimeSlots={availableTimeSlots}
       />
     </form>
   );
@@ -102,4 +140,5 @@ AppointmentForm.defaultProps = {
   salonOpensAt: 9,
   salonClosesAt: 19,
   today: new Date(),
+  availableTimeSlots: [],
 };
